b618c5e9398ce37e579749c15170cba6c822df92
AkumaKater <jan.fortowski@gmail.com>
2024-05-09 17:53:20 +0200

    Clean Up Imports

--- src/CNN/data/Image.java
+++ src/CNN/data/Image.java
@@ -7,9 +7,6 @@ import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 public class Image {
     
